57be143c46a688deb3a90bcbea659d0867207295
Savran Korkmaz <savrankorkmaz@hotmail.com>
2023-09-19 13:19:04 +0300

    New Commit

--- IKProject.DataAccessLayer/EntityFramework/EFApplicationDal.cs
+++ IKProject.DataAccessLayer/EntityFramework/EFApplicationDal.cs
@@ -2,13 +2,9 @@
 using IKProject.DataAccessLayer.Concrete;
 using IKProject.DataAccessLayer.Repository;
 using IKProject.EntityLayer.Concrete;
-using Microsoft.EntityFrameworkCore;
-using MigraDoc.Rendering;
-using PdfSharp.Pdf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,28 +15,16 @@ namespace IKProject.DataAccessLayer.EntityFramework
         public void UploadPdfFile(string fileName, byte[] fileContent)
         {
             using var _dbContext = new StoreContext();
-            var document = new Document();
-            var section = document.AddSection(); // Create a section
 
-            section.AddParagraph("This is a paragraph in the section.");
-
-            var renderer = new PdfDocumentRenderer();
-            renderer.Document = document;
-            renderer.RenderDocument();
-
-            using (var stream = new MemoryStream())
+            var pdfDocument = new Application
             {
-                renderer.Save(stream, false);
-                var pdfDocument = new Application
-                {
-                    FileName = fileName,
-                    PdfData = stream.ToArray(),
-                    ApplicationTime = DateTime.Now
-                };
+                FileName = fileName,
+                PdfData = fileContent,
+                ApplicationTime = DateTime.Now
+            };
 
-                _dbContext.PdfDocuments.Add(pdfDocument);
-                _dbContext.SaveChanges();
-            }
+            _dbContext.Applications.Add(pdfDocument);
+            _dbContext.SaveChanges();
         }
     }
 }
---
+++ IKProject.UI/Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
+using IKProject.EntityLayer.Concrete;
+
+namespace IKProject.WebUI.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DepartmentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7100/api/Department"); // Uygun port numarasını eklemeyi unutmayın
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var departments = JsonConvert.DeserializeObject<List<Department>>(jsonData);
+                return View(departments);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddDepartment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDepartment(Department department)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(department);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:7100/api/Department", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
